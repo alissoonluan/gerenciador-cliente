70e3ad15d7e75b3679316554b89acdaad78243d9
alissoonluan <alisson_luan_@hotmail.com>
2020-02-06 23:30:39 -0300

    front login

---
+++ cadastrar.php
@@ -0,0 +1,20 @@
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8"/>
+    <title>Projeto Login</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div id="body-form-Cad">
+        <h1>CADASTRAR</h1>
+        <form method="POST" action="process.php">
+            <input type="text" placeholder="Nome Completo">
+            <input type="text" placeholder="Telefone">
+            <input type="email" placeholder="Telefone">
+            <input type="password" placeholder="Senha">
+            <input type="password" placeholder="Confirmar Senha">
+            <input type="submit" value="CADASTRAR">
+        </form>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,18 @@
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8"/>
+    <title>Projeto Login</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div id="body-form">
+        <h1>ENTRAR</h1>
+        <form method="POST" action="process.php">
+            <input type="email" placeholder="Usuário">
+            <input type="password" placeholder="Senha">
+            <input type="submit" value="ACESSAR">
+            <a href="cadastrar.php">Ainda não é inscrito? <strong>Cadastre-se!</strong> 
+        </form>
+    </div>
+</body>
+</html>
